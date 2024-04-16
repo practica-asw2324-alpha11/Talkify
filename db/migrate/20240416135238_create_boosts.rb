e7b08d880325f837f37c3b85384d2200e932d63a
miguel.goni <miguel.goni@estudiantat.upc.edu>
2024-04-16 15:02:16 +0000

    boost

---
+++ db/migrate/20240416135238_create_boosts.rb
@@ -0,0 +1,10 @@
+class CreateBoosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :boosts do |t|
+      t.references :post, null: false, foreign_key: true
+      t.references :admin, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
