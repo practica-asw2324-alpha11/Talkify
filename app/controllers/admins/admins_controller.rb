16a44012ec7fc66a5c002dc8cd8130a888330627
sarachillon <sara.chillon@estudiantat.upc.edu>
2024-04-16 10:30:03 +0000

    tasks 42, 43, 45

--- app/controllers/admins/admins_controller.rb
+++ app/controllers/admins/admins_controller.rb
@@ -4,5 +4,32 @@ class Admins::AdminsController < ApplicationController
 
   def show
     @admin = Admin.find(params[:id])
+    params[:view] ||= 'threads'
+    @posts = @admin.posts.order(created_at: :desc)
+    @comments = @admin.comments.includes(:post).order(created_at: :desc)
+
   end
+
+  def edit
+    @admin = Admin.find(params[:id])
+  end
+
+  def update
+    @admin = Admin.find(params[:id])
+    if @admin.update(admin_params)
+      redirect_to admin_path(@admin), notice: 'Perfil actualizado con éxito.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    def set_admin
+       @admin = Admin.find(params[:id])
+    end
+    def admin_params
+        params.require(:admin).permit(:email, :full_name, :description, :avatar_url, :background_image, :uid)
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-  
-  
+
+
   devise_for :admins, controllers: {
     omniauth_callbacks: 'admins/omniauth_callbacks',
     sessions: 'admins/sessions'
   }
 
   resources :magazines
-  
+
   resources :posts do
 
       post 'upvote', on: :member
@@ -46,6 +46,9 @@ Rails.application.routes.draw do
     post 'admins/sign_in', to: 'admins/sessions#create', as: :admin_session
     get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
     get 'admins/:id', to: 'admins/admins#show', as: :admin
+    get 'admins/:id/edit', to: 'admins/admins#edit', as: :edit_admin
+    patch '/admins/:id', to: 'admins/admins#update'
+
   end
 
   # Añadir la ruta para el perfil del admin
