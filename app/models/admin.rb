1d92d72c2b9560b0cc37a3369715db723e377e15
Ivan <ivbuirez@gmail.com>
2024-04-18 22:04:27 +0200

    Fixed model admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -9,7 +9,7 @@ class Admin < ApplicationRecord
   has_one_attached :avatar
   has_one_attached :background
   
-  def self.from_google(email:, full_name:, uid:)
-    create_with(uid: uid, full_name: full_name).find_or_create_by!(email: email)
+  def self.from_google(email:, full_name:, uid:, avatar_url:)
+    create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(email: email)
   end
 end
