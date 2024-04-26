a1db56e20d1b6dede38a2f7f7579bf9295578f5c
FerranMartinezVives <ferran.martinez.vives@estudiantat.upc.edu>
2024-04-26 10:50:47 +0000

    validate form  magazines

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,4 +1,6 @@
 class Magazine < ApplicationRecord
+  validates :name, length: { minimum: 1 }
+  validates :title, length: { minimum: 1 }
   has_many :posts
   has_many :comments, through: :posts
   has_and_belongs_to_many :users
